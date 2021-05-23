7554333917121207bd649fd811232daefaf5c494
Sarunaskaz <82802216+Sarunaskaz@users.noreply.github.com>
2021-05-23 18:13:41 +0300

    socials

---
+++ js/home.js
@@ -0,0 +1,8 @@
+const logDOM = document.querySelector ('.logo');
+
+    function handleLogoClick () {
+        console.log ('Buvo paspaustas click');
+
+
+    }
+    logDOM.addEventListener('click', handleLogoClick);
---
+++ js/socials/socials.js
@@ -0,0 +1,5 @@
+function socials () {
+   for  
+}
+
+export { socials }
